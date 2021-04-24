49b714512316160ac409098d56bc866f327c736c
alemughal <alimug38@gmail.com>
2021-04-24 23:57:46 +0500

    Adding Images

---
+++ src/Components/QueenAnimations.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {PalmSmall1,BushSmall,PalmSmall3,KnightSmall,PawnSmall,PawanUpRightSmall,RunningQueen,Tree,RookSmall,RookUprightSmall} from '../Images/';
+
+function QueenAnimations() {
+    return (
+        <div className="Wrapper">
+            <div className="sky"></div>
+            <div className="earth">
+                <div id="red-queen_and_alice">
+                    <img src={RunningQueen} alt="RunningQueen"/>
+                </div>
+            </div>
+
+            <div className="scenery" id="foreground1">
+                <img id="palm3" src={PalmSmall3} alt="Palm"/>
+            </div>
+
+            <div className="scenery" id="foreground2">
+                <img id="bush" src={BushSmall} alt="Bush"/>
+                <img src={RookUprightSmall} alt="RookUpright"/>
+            </div>
+
+            <div className="scenery" id="background1">
+                <img id="r_pawn_upright" src={PawanUpRightSmall} alt=""/>
+                <img id="w_rook" src={RookSmall} alt=""/>
+                <img id="palm1" src={PalmSmall1} alt=""/>
+            </div>
+
+            <div className="scenery" id="background2">
+                <img id="r_pawn" src={PawnSmall} alt="Pawn"/>
+                <img id="r_knight" src={KnightSmall} alt="Knight"/>
+                <img id="palm2" src={Tree} alt="Tree"/>
+            </div>
+        </div>
+    )
+    }
+
+
+export default QueenAnimations;
---
+++ src/Images/index.js
@@ -0,0 +1,12 @@
+export {default as PalmSmall1} from '../Images/palm1_small.png';
+export {default as BushSmall} from '../Images/bush_small.png';
+export {default as PalmSmall3} from '../Images/palm3_small.png';
+export {default as KnightSmall} from '../Images/r_knight_small.png';
+export {default as PawnSmall} from '../Images/r_pawn_small.png';
+export {default as PawanUpRightSmall} from '../Images/r_pawn_upright_small.png';
+export {default as RunningQueen} from '../Images/sprite_running-alice-queen_small.png';
+export {default as Tree} from '../Images/tree.png';
+export {default as RookSmall} from '../Images/w_rook_small.png';
+export {default as RookUprightSmall} from '../Images/w_rook_upright_small.png';
+
+
